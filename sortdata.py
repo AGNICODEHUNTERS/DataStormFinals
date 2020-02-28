c6eb7fe6a8aac9474b16ded7bac861ea3b1aea9b
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-29 03:11:11 +0530

    head

--- sortdata.py
+++ sortdata.py
@@ -7,3 +7,7 @@ import seaborn as sns
 customer_data = pd.read_csv("customer_data.csv")
 sales_detail = pd.read_csv("sales_detal.csv")
 sales_summary= pd.read_csv("sales_summary.csv")
+
+print(customer_data.head())
+print(sales_detail.head())
+print(sales_summary.head())
