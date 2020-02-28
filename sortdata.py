ec943d8734f7c5abffe82ca6c6508d454c36fb1f
crezix <ireshmadhusankka@gmail.com>
2020-02-29 04:22:48 +0530

    cr

--- sortdata.py
+++ sortdata.py
@@ -6,6 +6,7 @@ import seaborn as sns
 
 customer_data = pd.read_csv("customer_data.csv").sort_values('customer_id')
 sales_detail = pd.read_csv("sales_detal.csv").sort_values(['customer_id','year','month'],ascending=True)
+sales_detail = pd.get_dummies(sales_detail,columns=['catagory_code'],prefix='',prefix_sep='')
 sales_summary= pd.read_csv("sales_summary.csv").sort_values(['customer_id','year','month'],ascending=True)
 
 print(customer_data.head())
