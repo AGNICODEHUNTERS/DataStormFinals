e34bf73dde13f64d1c19b7108994656713129e96
crezix <ireshmadhusankka@gmail.com>
2020-02-29 03:25:18 +0530

    cr2

--- sortdata.py
+++ sortdata.py
@@ -5,8 +5,8 @@ import scipy.stats as stats
 import seaborn as sns
 
 customer_data = pd.read_csv("customer_data.csv").sort_values('customer_id')
-sales_detail = pd.read_csv("sales_detal.csv").sort_values('customer_id','year','month')
-sales_summary= pd.read_csv("sales_summary.csv").sort_values('customer_id','year','month')
+sales_detail = pd.read_csv("sales_detal.csv").sort_values(['customer_id','year','month'],ascending=True)
+sales_summary= pd.read_csv("sales_summary.csv").sort_values(['customer_id','year','month'],ascending=True)
 
 print(customer_data.head())
 print(sales_detail.head())
