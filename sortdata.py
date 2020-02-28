57e0cfeb66d050ef76c63ef48d41c2d1dc7b1b88
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-29 02:51:52 +0530

    import

---
+++ sortdata.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import seaborn as sns
+
+customer_data = pd.read_csv("customer_data.csv")
+sales_detail = pd.read_csv("sales_detal.csv")
+sales_summary= pd.read_csv("sales_summary.csv")
