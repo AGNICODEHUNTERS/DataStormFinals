f27f2f13d00339c648f08ef051c1d047349ab6c6
crezix <ireshmadhusankka@gmail.com>
2020-02-29 09:34:44 +0530

    cr

--- sortdata.py
+++ sortdata.py
@@ -9,11 +9,14 @@ sales_detail = pd.read_csv("sales_detal.csv").sort_values(['customer_id','year',
 sales_detail = pd.get_dummies(sales_detail,columns=['catagory_code'],prefix='',prefix_sep='')
 sales_summary= pd.read_csv("sales_summary.csv").sort_values(['customer_id','year','month'],ascending=True)
 
-sales = pd.merge_asof(sales_detail,sales_summary, on='customer_id', by =['year','month','end_date'])
-sales = pd.merge_asof(customer_data,sales,on='customer_id')
+
+sales_summary = sales_summary.groupby('customer_id')
+sales = sales_summary.get_group(167939)
+'''sales = pd.merge_asof(right = sales_detail,left = sales_summary, on='customer_id', by =['year','month','end_date'])
+sales = pd.merge_asof(sales,customer_data,on='customer_id')
 print(customer_data.head())
 print(sales_detail.head())
 print(sales_summary.head())
-print(sales.head())
+print(sales.head())'''
 
 sales.to_csv('a.csv',index=False)
