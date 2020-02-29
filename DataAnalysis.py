9c474acd808086eb5549a76f72e1204e00094919
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-29 10:56:44 +0530

    update

---
+++ DataAnalysis.py
@@ -0,0 +1,261 @@
+# here we will import the libraries used for machine learning
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from scipy.stats import randint
+import pandas as pd # data processing, CSV file I/O, data manipulation
+import matplotlib.pyplot as plt # this is used for the plot the graph
+import seaborn as sns # used for plot interactive graph.
+from pandas import set_option
+plt.style.use('ggplot') # nice plots
+
+from sklearn.model_selection import train_test_split # to split the data into two parts
+from sklearn.linear_model import LogisticRegression # to apply the Logistic regression
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import KFold # for cross validation
+from sklearn.model_selection import GridSearchCV # for tuning parameter
+from sklearn.model_selection import RandomizedSearchCV  # Randomized search on hyper parameters.
+from sklearn.preprocessing import StandardScaler # for normalization
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+#import xgboost as xgb
+#from xgboost import XGBClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier #KNN
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn import metrics # for the check the error and accuracy of the model
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+import os
+#print(os.listdir("../input"))
+
+data = pd.read_csv('train_summary.csv')
+testData= pd.read_csv('test_summary.csv')
+'''
+def numeric(dataSheet):
+    bal = dataSheet.Balance_Limit_V1
+    gen = dataSheet.Gender
+    edu = dataSheet.EDUCATION_STATUS
+    mar = dataSheet.MARITAL_STATUS
+    age = dataSheet.AGE
+
+    i=0
+
+    balF = []
+    genF = []
+    eduF = []
+    marF = []
+    ageF = []
+
+    for m in range(len(bal)):
+        if bal.iloc[m].endswith('M'):
+            balF.append(float(bal[m][:-1])*1000000)
+        elif bal.iloc[m].endswith('K'):
+            balF.append(float(bal[m][:-1])*1000)
+        else:
+            balF.append(float(bal[m]))
+
+    while True:
+        try:
+            if str(gen.iloc[i])=="M":
+                genF.append(1)
+            else:
+                genF.append(2)
+
+            if str(edu.iloc[i])=="Graduate":
+                eduF.append(1)
+            elif str(edu.iloc[i])=="High School":
+                eduF.append(2)
+            else:
+                eduF.append(3)
+
+            if str(mar.iloc[i])=="Single":
+                marF.append(1)
+
+            else:
+                marF.append(2)
+
+            if str(age.iloc[i])=="31-45":
+                ageF.append(1)
+            elif str(age.iloc[i])=="46-65":
+                ageF.append(2)
+            else:
+                ageF.append(3)
+            i=i+1
+        except:
+            dataSheet.insert(1,"balF",balF)
+            dataSheet.insert(2,"genF",genF)
+            dataSheet.insert(3,"eduF",eduF)
+            dataSheet.insert(4,"marF",marF)
+            dataSheet.insert(5,"ageF",ageF)
+            break
+    return dataSheet'''
+
+print(data.head())
+data.rename(columns={"NEXT_MONTH_DEFAULT": "Default"}, inplace=True)
+#print(data.sample(5))
+data.drop('Client_ID', axis = 1, inplace =True) # drop column "ID"
+#print(data.info())
+
+# Separating features and target
+y = data.Default     # target default=1 or non-default=0
+features = data.drop('Default', axis = 1, inplace = False)
+print(data)
+###data=pd.get_dummies(data)
+
+###merging of data###
+'''
+data['EDUCATION']=np.where(data['EDUCATION'] == 5, 4, data['EDUCATION'])
+data['EDUCATION']=np.where(data['EDUCATION'] == 6, 4, data['EDUCATION'])
+data['EDUCATION']=np.where(data['EDUCATION'] == 0, 4, data['EDUCATION'])
+
+data['MARRIAGE'].unique()
+data['MARRIAGE']=np.where(data['MARRIAGE'] == 0, 3, data['MARRIAGE'])
+data['MARRIAGE'].unique()'''
+
+                        ###EDA###
+
+###The frequency of defaults###
+yes = data.Default.sum()
+no = len(data)-yes
+
+###Percentage###
+yes_perc = round(yes/len(data)*100, 1)
+no_perc = round(no/len(data)*100, 1)
+
+import sys
+plt.figure(figsize=(7,4))
+sns.set_context('notebook', font_scale=1.2)
+sns.countplot('Default',data=data, palette="Blues")
+plt.annotate('Non-default: {}'.format(no), xy=(-0.3, 15000), xytext=(-0.3, 3000), size=12)
+plt.annotate('Default: {}'.format(yes), xy=(0.7, 15000), xytext=(0.7, 3000), size=12)
+plt.annotate(str(no_perc)+" %", xy=(-0.3, 15000), xytext=(-0.1, 8000), size=12)
+plt.annotate(str(yes_perc)+" %", xy=(0.7, 15000), xytext=(0.9, 8000), size=12)
+plt.title('COUNT OF CREDIT CARDS', size=14)
+####Removing the frame###
+plt.box(False);
+
+###Summary Statistics###
+set_option('display.width', 100)
+set_option('precision', 2)
+
+print("SUMMARY STATISTICS OF NUMERIC COLUMNS")
+print()
+print(data.describe().T)
+
+###Creating a new dataframe with categorical variables###
+subset = data[['Gender', 'EDUCATION_STATUS', 'MARITAL_STATUS', 'PAY_JULY', 'PAY_AUG', 'PAY_SEP', 'PAY_OCT',
+               'PAY_NOV', 'PAY_DEC', 'Default']]
+
+f, axes = plt.subplots(3, 3, figsize=(35, 30), facecolor='white')
+f.suptitle('FREQUENCY OF CATEGORICAL VARIABLES (BY TARGET)')
+ax1 = sns.countplot(x="Gender", hue="Default", data=subset, palette="Blues", ax=axes[0,0])
+ax2 = sns.countplot(x="EDUCATION_STATUS", hue="Default", data=subset, palette="Blues",ax=axes[0,1])
+ax3 = sns.countplot(x="MARITAL_STATUS", hue="Default", data=subset, palette="Blues",ax=axes[0,2])
+ax4 = sns.countplot(x="PAY_JULY", hue="Default", data=subset, palette="Blues", ax=axes[1,0])
+ax5 = sns.countplot(x="PAY_AUG", hue="Default", data=subset, palette="Blues", ax=axes[1,1])
+ax6 = sns.countplot(x="PAY_SEP", hue="Default", data=subset, palette="Blues", ax=axes[1,2])
+ax7 = sns.countplot(x="PAY_OCT", hue="Default", data=subset, palette="Blues", ax=axes[2,0])
+ax8 = sns.countplot(x="PAY_NOV", hue="Default", data=subset, palette="Blues", ax=axes[2,1])
+ax9 = sns.countplot(x="PAY_DEC", hue="Default", data=subset, palette="Blues", ax=axes[2,2]);
+
+###PLotting a histogram###
+x1 = list(data[data['Default'] == 1]['balF'])
+x2 = list(data[data['Default'] == 0]['balF'])
+print(x1)
+plt.figure(figsize=(10,6))
+sns.set_context('notebook', font_scale=1.2)
+#sns.set_color_codes("pastel")
+plt.hist([x1, x2], bins = 40, normed=False, color=['steelblue', 'lightblue'])
+plt.xlim([0,600000])
+plt.legend(['Yes', 'No'], title = 'Default', loc='upper right', facecolor='white')
+plt.xlabel('Limit Balance (NT dollar)')
+plt.ylabel('Frequency')
+plt.title('LIMIT BALANCE HISTOGRAM BY TYPE OF CREDIT CARD', SIZE=15)
+plt.box(False)
+#plt.savefig('ImageName', format='png', dpi=200, transparent=True);
+
+###BOXPLOT###
+Repayment = data[['PAY_JULY', 'PAY_AUG', 'PAY_SEP', 'PAY_OCT', 'PAY_NOV', 'PAY_DEC']]
+
+Repayment = pd.concat([y,Repayment],axis=1)
+Repayment = pd.melt(Repayment,id_vars="Default",
+                    var_name="Repayment_Status",
+                    value_name='value')
+
+plt.figure(figsize=(10,5))
+sns.set_context('notebook', font_scale=1.2)
+sns.boxplot(y="value", x="Repayment_Status", hue="Default", data=Repayment, palette='Blues')
+plt.legend(loc='best', title= 'Default', facecolor='white')
+plt.xlim([-1.5,5.5])
+plt.title('REPAYMENT STATUS - BOXPLOT', size=14)
+plt.box(False)
+plt.savefig('ImageName', format='png', dpi=200);
+
+## data are distributed in a wide range (below), need to be normalizded.
+plt.figure(figsize=(15,3))
+ax= data.drop('Default', axis=1).boxplot(data.columns.name, rot=90)
+outliers = dict(markerfacecolor='b', marker='p')
+ax= features.boxplot(features.columns.name, rot=90, flierprops=outliers)
+plt.xticks(size=12)
+ax.set_ylim([-5000,100000])
+plt.box(False);
+'''
+###standardization###
+print(type(features.values))
+stdX = (features.values) - (features.values).mean() / (features.values).std()
+data_st = pd.concat([y,stdX.iloc[:,:]],axis=1)
+data_st = pd.melt(data_st,id_vars="Default",
+                    var_name="features",
+                    value_name='value')'''
+
+plt.figure(figsize=(20,10))
+sns.set_context('notebook', font_scale=1)
+sns.violinplot(y="value", x="features", hue="Default", data=data_st,split=True,
+               inner="quart", palette='Blues')
+plt.legend(loc=4, title= 'Default', facecolor='white')
+plt.ylim([-3,3])
+plt.title('STANDARDIZED FEATURES - VIOLIN PLOT', size=14)
+plt.box(False)
+plt.savefig('ImageName', format='png', dpi=200, transparent=False)
+
+#  looking at correlations matrix, defined via Pearson function
+corr = data.corr() # .corr is used to find corelation
+f,ax = plt.subplots(figsize=(8, 7))
+sns.heatmap(corr, cbar = True,  square = True, annot = False, fmt= '.1f',
+            xticklabels= True, yticklabels= True
+            ,cmap="coolwarm", linewidths=.5, ax=ax)
+plt.title('CORRELATION MATRIX - HEATMAP', size=18);
+plt.show()
+'''
+sns.lmplot(x='Default', y= 'PAY_JULY', data = data, hue ='Default',palette='coolwarm')
+plt.title('Linear Regression: distinguishing between Default and Non-default', size=16)
+sns.lmplot(x='Default', y= 'PAY_AUG', data = data, hue ='Default',palette='coolwarm')
+plt.title('Linear Regression: Cannot distinguish between Default and Non-default', size=16);
+print('Uncorrelated data are poentially more useful: discrimentory!')'''
+
+# Original dataset
+X = data.drop('Default', axis=1)
+y = data['Default']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, stratify=y, random_state=42)
+# Dataset with standardized features
+Xstd_train, Xstd_test, ystd_train, ystd_test = train_test_split(stdX,y, test_size=0.2, stratify=y,random_state=42)
+
+NUM_FEATURES = 3
+model = LogisticRegression()
+rfe_stand = RFE(model, NUM_FEATURES)
+fit_stand = rfe_stand.fit(X, y)
+#print("St Model Num Features:", fit_stand.n_features_)
+#print("St Model Selected Features:", fit_stand.support_)
+print("Std Model Feature Ranking:", fit_stand.ranking_)
+# calculate the score for the selected features
+score_stand = rfe_stand.score(X,y)
+print("Standardized Model Score with selected features is: %f (%f)" % (score_stand.mean(), score_stand.std()))
+
+feature_names = np.array(features.columns)
+print('Most important features (RFE): %s'% feature_names[rfe_stand.support_])
